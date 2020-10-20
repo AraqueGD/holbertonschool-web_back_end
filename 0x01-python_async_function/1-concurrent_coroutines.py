1ae31607a0cc02254080e924aca5859433196e80
AraqueGD <camiaraquecaro17@gmail.com>
2020-10-20 15:11:04 -0500

    [ADD]1. Let's execute multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python3
 """ Import Modules """
-
 import asyncio
 import random
 from typing import List
 
-wait_random = __import__("0-basic_async_syntax").wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Append Times in List """
-    queue, array = [], []
-
-    # for _ in range(n):
-    #     queue.append(asyncio.create_task(wait_random(max_delay)))
-
-    # array.append(await asyncio.gather(*queue))
-
-    # for q in asyncio.as_completed(queue):
-    #     array.append(await q)
+    q, array = [], []
 
     for _ in range(n):
-        queue.append(wait_random(max_delay))
+        q.append(wait_random(max_delay))
 
-    for q in asyncio.as_completed(queue):
-        result = await q
-        array.append(result)
+    for idx in asyncio.as_completed(q):
+        rta = await idx
+        array.append(rta)
 
     return array
