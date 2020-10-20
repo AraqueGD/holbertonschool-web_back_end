38eb0f4ee283fb3edd71920362b02b67a6f46855
AraqueGD <camiaraquecaro17@gmail.com>
2020-10-20 13:04:22 -0500

    [ADD]12. Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" Mypy Function """
+""" Type Checking  """
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """ Valid mypy Function """
+    """ Zoom Array """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
@@ -12,7 +12,7 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
