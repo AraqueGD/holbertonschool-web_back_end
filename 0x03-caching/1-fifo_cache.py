81efbd0af97797286380b41c5f3be9b79978bcf1
AraqueGD <camiaraquecaro17@gmail.com>
2020-10-26 17:47:43 -0500

    [ADD]1. FIFO caching

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -16,17 +16,20 @@ class FIFOCache(BaseCaching):
         """ Method Put """
         if (key is None or item is None):
             return
-        self.cache_data[key] = item
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
-            if (self.list_cache):
-                l = self.list_cache.pop()
-                del self.cache_data[l]
-                print("DISCARD: {}".format(l))
 
         if (key not in self.list_cache):
             self.list_cache.append(key)
         else:
             self.l_list(key)
+
+        self.cache_data[key] = item
+
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            first = self.get_first(self.list_cache)
+            if (first):
+                self.list_cache.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
 
     def get(self, key):
         """ Method Get """
@@ -38,3 +41,8 @@ class FIFOCache(BaseCaching):
         if self.list_cache[length - 1] != item:
             self.list_cache.remove(item)
             self.list_cache.append(item)
+
+    @staticmethod
+    def get_first(array):
+        """ Get First Item """
+        return array[0] if array else None
