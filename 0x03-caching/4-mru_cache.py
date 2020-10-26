cc8b31a018071cadfbe47d107975d9b8aece59a6
AraqueGD <camiaraquecaro17@gmail.com>
2020-10-26 18:04:47 -0500

    [ADD]4. MRU Caching

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" MRU Cache """
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ Class MRUCache """
+
+    def __init__(self):
+        """ Cosntructor """
+        super().__init__()
+        self.list_cache = []
+
+    def put(self, key, item):
+        """ Method Put """
+        if (key is None or item is None):
+            return
+
+        self.cache_data[key] = item
+
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            if (self.list_cache):
+                l = self.list_cache.pop()
+                del self.cache_data[l]
+                print("DISCARD: {}".format(l))
+
+        if (key not in self.list_cache):
+            self.list_cache.append(key)
+        else:
+            self.l_list(key)
+
+    def get(self, key):
+        """ Method Get """
+        item = self.cache_data.get(key, None)
+        if (item is not None):
+            self.l_list(key)
+        return item
+
+    def l_list(self, item):
+        """ Method Lasted List Item """
+        length = len(self.list_cache)
+        if self.list_cache[length - 1] != item:
+            self.list_cache.remove(item)
+            self.list_cache.append(item)
