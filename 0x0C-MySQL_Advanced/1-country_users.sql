798cd5294633f310b78e8dea38dec5f4b0b2ebe1
AraqueGD <camiaraquecaro17@gmail.com>
2020-12-02 17:55:41 -0500

    [ADD]1. In and not out

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- 1. In and not out
+-- Write a SQL script that creates a table users following these requirements.
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM("US", "CO", "TN") NOT NULL
+    PRIMARY KEY(id)
+);
